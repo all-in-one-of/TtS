88fb6f8f291312ea08a20b9297228225bbd3b6c5
Oscar Louw <ojlou1@student.monash.edu>
2018-01-22 14:32:51 +1100

    Basic Navmesh-based character controller

---
+++ Assets/NavMeshCharacterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshCharacterController : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+	// Use this for initialization
+	void Start ()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        Vector3 movementDirection = new Vector3(Input.GetAxisRaw("HorizontalWASD"), 0, Input.GetAxisRaw("VerticalWASD"));
+
+
+            navMeshAgent.SetDestination(transform.position + movementDirection.normalized * 1);
+		
+	}
+}
